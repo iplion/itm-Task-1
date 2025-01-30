98d27fcc2b268ccc6ab8c8456ff06c39ec4b77bd
Lev Evseev <lionevseev@gmail.com>
2025-01-30 23:01:55 +0700

    solved the issue

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,94 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private final Connection connection;
 
+    public UserDaoJDBCImpl() throws SQLException {
+        this.connection = Util.getConnection();
+    }
+
+    private void makeQuery(String query) {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.out.printf("Error making query: %s", e.getMessage());
+        }
     }
 
     public void createUsersTable() {
-
+        makeQuery("CREATE TABLE IF NOT EXISTS users (" +
+            "id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, " +
+            "name VARCHAR(50), " +
+            "lastName VARCHAR(50), " +
+            "age SMALLINT)");
     }
 
     public void dropUsersTable() {
-
+        makeQuery("DROP TABLE IF EXISTS users");
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String query = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
 
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setByte(3, age);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void removeUserById(long id) {
+        String query = "DELETE FROM users WHERE id=?";
 
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+
+                users.add(new User(id, name, lastName, age));
+            }
+        } catch (SQLException e) {
+            System.out.printf("Error getting users: %s", e);
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
+        makeQuery("TRUNCATE TABLE users");
+    }
 
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.printf("Error closing connection: %s", e.getMessage());
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/itm-db";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
